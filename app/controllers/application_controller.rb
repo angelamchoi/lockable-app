d99dd7e3085804febccc985f89059aa5995465a6
Angela Choi <angelamchoi>
2024-10-05 18:39:21 -0400

    add sign in and sign out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
-end
+    protect_from_forgery with: :exception
+    before_action :configure_permitted_parameters, if: :devise_controller?
+  
+    protected
+    
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up) do |user|
+        user.permit(:name, :email, :password)
+      end
+      devise_parameter_sanitizer.permit(:account_update) do |user|
+        user.permit(:name, :email, :password, :current_password)
+      end
+    end
+  end
